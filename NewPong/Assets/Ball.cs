1a0907902ea0d0be30b794296cfdb8be13255810
Joshhh <jobayonovd@gmail.com>
2020-09-10 14:20:35 +0200

    Commit Base + Game Basics

---
+++ NewPong/Assets/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    int speedX; 
+    int speedY; 
+
+    float speed;
+
+    // Start is called before the first frame update
+    void Start(){
+
+        speed = Random.Range(5, 10);
+
+        speedX = Random.Range(0, 2);
+        if(speedX == 0){
+            speedX = 1; 
+        }else{
+            speedX = -1;
+        }
+
+        speedY = Random.Range(0, 2); 
+        if (speedY == 0){
+            speedY = 1;
+        }else{
+            speedY = -1; 
+        }
+
+        GetComponent<Rigidbody>().velocity = new Vector3(speed * speedX, speed * speedY, 0);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ NewPong/Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public KeyCode up; //Clase que define las teclas
+
+    public KeyCode down; 
+    // Start is called before the first frame update
+
+    float speed; 
+
+    void Start()
+    {
+        speed = 0.15f; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(up)){
+            transform.Translate(0, speed, 0);
+        }
+
+        if(Input.GetKey(down)){
+            transform.Translate(0, -speed, 0);
+        }
+        
+    }
+}
